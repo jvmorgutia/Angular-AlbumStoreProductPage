088798077a515ccfaefdb14903b8abf434a00ad8
Jesse Morgutia <jvmorgutia@gmail.com>
2018-02-02 14:23:01 -0800

    Merged import statements.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,7 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component'
-import { RouterModule } from '@angular/router'
-import { Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router'
 
 const appRoutes: Routes = [
   {path:'products', component: ProductListComponent},
